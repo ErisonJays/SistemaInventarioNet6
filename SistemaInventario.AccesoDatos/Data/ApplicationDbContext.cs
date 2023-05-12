1c4bb0e08da3af706f3a73997acd8316627ca7b5
erisonjays <erisonpinedastudent@gmail.com>
2023-05-12 12:34:29 -0400

    Configuracion del Proyecto

---
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos;
+using System.Reflection;
+
+namespace SistemaInventarioNet6.AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bodega> Bodegas { get; set; } // Bodega es el nombre del modelo y Bodegas es como se va a guardar en la base de datos
+
+        //creamos un override a un metodo que ya existe para usar Fluent API
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nombre es Requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre no puede superar el maximo de 60 caracteres")]
+        public string  Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripcion es Requerido")]
+        [MaxLength(60, ErrorMessage = "Descripcion no puede superar el maximo de 60 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required (ErrorMessage ="Estado es Requerido")]
+        public bool Estado { get; set; }
+    }
+}
